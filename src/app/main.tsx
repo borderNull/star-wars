da1d2b1d5a8e5fa96d4df3ca4ceb4fa3323310d9
borderNull <spbtvtower@gmail.com>
2024-07-11 13:54:21 +0300

    add bounaday

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,17 +6,37 @@ import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, Link } from "react-router-dom";
 import { router } from './router'
+import { ROUTES } from "./routes";
 import { queryClient } from "./query-client";
+import { ErrorBoundary } from "react-error-boundary";
+
+const Fallback = ({ error, resetErrorBoundary }) => {
+  console.log("🚀 ~ Fallback ~ error:", error)
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <Link to={ROUTES.LIST}>to list</Link>
+    </div>
+  );
+}
+
 
 import "./index.css";
 
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={Fallback}>
+
+
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
